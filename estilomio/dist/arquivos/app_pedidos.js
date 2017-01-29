59f4cc46480c314995806165291d7703417acfe9
Ani <anareymej@gmail.com>
2017-01-29 16:47:00 -0500

    feat(demos): project added

---
+++ estilomio/dist/arquivos/app_pedidos.js
@@ -0,0 +1,7 @@
+var app;
+
+app = angular.module('myapp', []);
+
+app.controller('pedidosController');
+
+alert("dentro");
---
+++ estilomio/src/preprocessors/coffee/app_pedidos.coffee
@@ -0,0 +1,5 @@
+app = angular.module 'myapp', []
+
+
+app.controller 'pedidosController'
+alert("dentro")
